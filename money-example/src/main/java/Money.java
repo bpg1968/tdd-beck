320f83d75d04eeed465ebe7862d18ad0c2b36d94
Brian P. Giroux <bgiroux@laurentian.ca>
2018-05-15 22:03:10 -0400

    moved CHF currency type to static factory method

--- money-example/src/main/java/Money.java
+++ money-example/src/main/java/Money.java
@@ -17,7 +17,7 @@ abstract class Money {
   }
 
   static Money franc(int amount) {
-    return new Franc(amount);
+    return new Franc(amount, "CHF");
   }
 
   abstract Money times(int multiplier);
