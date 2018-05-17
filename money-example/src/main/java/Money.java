5a51c0e523c2be68a58cd01912da22824973e958
Brian P. Giroux <bgiroux@laurentian.ca>
2018-05-16 21:30:28 -0400

    moved times() up to Money

--- money-example/src/main/java/Money.java
+++ money-example/src/main/java/Money.java
@@ -1,4 +1,4 @@
-abstract class Money {
+class Money {
   protected int amount;
   protected String currency;
 
@@ -14,7 +14,7 @@ abstract class Money {
   public boolean equals(Object object) {
     Money money = (Money) object;
     return amount == money.amount
-      && getClass().equals(money.getClass());
+      && currency().equals(money.currency());
   }
 
   static Money dollar(int amount) {
@@ -25,5 +25,11 @@ abstract class Money {
     return new Franc(amount, "CHF");
   }
 
-  abstract Money times(int multiplier);
+  Money times(int multiplier) {
+    return new Money(amount * multiplier, currency);
+  }
+
+  public String toString() {
+    return amount + " " + currency;
+  }
 }
